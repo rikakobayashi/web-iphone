08274dbb15515943d14b0ae6afc663387b47fa7f
小林莉華 <candypops0530@yahoo.co.jp>
2020-06-15 20:03:04 +0900

    fix

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ app.use(express.static(path.join(__dirname, 'dist')))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'), {})
 })
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+})
